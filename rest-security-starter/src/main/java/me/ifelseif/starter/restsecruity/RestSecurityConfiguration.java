d63847ef38587380c47a9a1de9d022aaf4918832
林鸣昶 <517288724@qq.com>
2019-11-07 15:07:32 +0800

    临时增加不鉴权接口,需优化

--- rest-security-starter/src/main/java/me/ifelseif/starter/restsecruity/RestSecurityConfiguration.java
+++ rest-security-starter/src/main/java/me/ifelseif/starter/restsecruity/RestSecurityConfiguration.java
@@ -69,7 +69,8 @@ public class RestSecurityConfiguration extends WebSecurityConfigurerAdapter {
                         "/**/*.js"
                 ).permitAll()
                 // 对于获取token的rest api要允许匿名访问
-                .antMatchers("/app/auth/**", "/app/coin_put_in/**", "/admin/auth/**", "/autoload-cache/**").permitAll()
+                //todo 可配置
+                .antMatchers("/app/aex/**", "/app/auth/**", "/app/coin_put_in/**", "/admin/auth/**", "/autoload-cache/**").permitAll()
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated();
 
